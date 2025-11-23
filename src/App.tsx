1a024e2312cb9e3a97d8c739b665971665af4f81
pawel83gt <80663450+pawel83gt@users.noreply.github.com>
2025-11-23 19:57:09 +0700

    добавлен TrackItem

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import { TrackList } from './components/TrackList';
 import { TrackDetail } from './components/TrackDetail';
 
 function App() {
-
   const [selectedTrack, setSelectedTrack] = useState(null);
+
+  const handleTrackSelect = (id) => {
+    setSelectedTrack(id);
+  };
   return (
     <div className="general-block">
-      <TrackList onSelectTrack={setSelectedTrack} />
+      <TrackList
+        onSelectTrack={handleTrackSelect}
+        selectTrackId={selectedTrack}
+      />
       <TrackDetail id={selectedTrack} />
     </div>
   );
--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,75 +1,66 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { TrackItem } from './TrackItem';
 
 interface Track {
-    id: number;
-    attributes: {
-        title: string;
-        attachments: [
-            {
-                url: string;
-            }
-        ];
+  id: number;
+  attributes: {
+    title: string;
+    attachments: [
+      {
+        url: string;
+      }
+    ];
+  };
+}
+
+export const TrackList = ({ onSelectTrack, selectTrackId }) => {
+  const [data, setData] = useState<Track[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://musicfun.it-incubator.app/api/1.0/playlists/tracks',
+        {
+          headers: {
+            'api-key': 'bec399a0-ffa4-41d0-89c1-d7badc2ab952',
+          },
+        }
+      ).then((response) => response.json());
+      setData(response.data);
     };
-}
 
-export const TrackList = ({ onSelectTrack }) => {
+    fetchData();
+  }, []);
 
-    const [data, setData] = useState<Track[] | null>(null);
-    const [selectTrackId, setSelectTrackId] = useState<number>();
+  const handleClick = (trackId) => {
+    onSelectTrack?.(trackId);
+  };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(
-                'https://musicfun.it-incubator.app/api/1.0/playlists/tracks',
-                {
-                    headers: {
-                        'api-key': 'bec399a0-ffa4-41d0-89c1-d7badc2ab952',
-                    },
-                }
-            ).then((response) => response.json());
-            setData(response.data);
-        };
-
-        fetchData();
-    }, []);
-
-    if (!data) {
-        return (
-            <div className="center-block">
-                <div className="gradient-loader"></div>
-            </div>
-        );
-    }
-
+  if (!data) {
     return (
-        <>
-            <div className="wrapper">
-                <h1>Musicfun Player!</h1>
-                <ul>
-                    {data.map((track) => {
-                        return (
-                            <li>
-                                <div
-                                    key={track.id}
-                                    onClick={() => {
-                                        setSelectTrackId(track.id);
-                                        onSelectTrack(track.id);
-                                    }}
-                                >
-                                    <div className={selectTrackId === track.id ? 'active' : ''}>
-                                        {track.attributes.title}
-                                    </div>
-                                    <audio
-                                        src={track.attributes.attachments[0].url}
-                                        controls
-                                    ></audio>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        </>
+      <div className="center-block">
+        <div className="gradient-loader"></div>
+      </div>
+    );
+  }
 
-    )
-}
+  return (
+    <>
+      <div className="wrapper">
+        <h1>Musicfun Player!</h1>
+        <ul>
+          {data.map((track) => {
+            return (
+              <TrackItem
+                key={track.id}
+                track={track}
+                isSelectedTrack={track.id === selectTrackId}
+                onSelect={handleClick}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
